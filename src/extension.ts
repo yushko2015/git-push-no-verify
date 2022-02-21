3cc64fdf52c8265b68c5123c185ca86e4a87978e
yushko2015 <andrjj.jusho@gmail.com>
2022-02-21 11:44:40 +0200

    Update extension.ts

--- src/extension.ts
+++ src/extension.ts
@@ -1,18 +1,39 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
-const git  = require('gift')
+const git  = require('gift');
 
 export function activate(context: vscode.ExtensionContext) {
 	let disposable = vscode.commands.registerCommand('git-push-no-verify.gitPushNoVerify', () => {
-		const repo = git(vscode.workspace.rootPath);
+		const gitExtension = vscode.extensions.getExtension('vscode.git')?.exports;
+		const gitApi = gitExtension.getAPI(1);
+
+		if (!gitApi) {
+			return;
+		}
+
+		const folders = vscode.workspace.workspaceFolders;
+		const folderPath = folders[0].uri.path;
+		let selectedRepository = gitApi.repositories.find((repository: any) => {
+			return repository.rootUri.path === folderPath;
+		});
+
+		if(!selectedRepository) {
+			return;
+		}
+
+		const repo = git(selectedRepository.rootUri.path);
+		let remoteName = selectedRepository.state.HEAD?.upstream?.remote;
+
 		vscode.commands.executeCommand('git.pull').then(() => {
-			repo.remote_push('origin', (e: string | any) => {
+			repo.remote_push(remoteName, ['-u --no-verify'], (e: string | any) => {
 				if(!e) {
 					vscode.commands.executeCommand('git.refresh');
 					vscode.window.showInformationMessage('Changes pushed to origin without verification!');
+				} else {
+					console.log(e);
 				}
-			})
+			});
 		});
 	});
 
